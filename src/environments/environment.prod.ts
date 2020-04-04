0cad4be2cf6c8bf1ff3088de475e018f07b218f2
ahmer-najam <53119340+ahmer-najam@users.noreply.github.com>
2020-04-04 19:16:24 +0500

    InitCommit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDg4vNTROrlg_SpRY9pPhUlS9NJw8gmhh8',
+    authDomain: 'my-shop-54aa3.firebaseapp.com',
+    databaseURL: 'https://my-shop-54aa3.firebaseio.com',
+    projectId: 'my-shop-54aa3',
+    storageBucket: 'my-shop-54aa3.appspot.com',
+    messagingSenderId: '924464511290',
+    appId: '1:924464511290:web:08aa7ccc8da6fc084a7803',
+    measurementId: 'G-BDNYZBPMZV',
+  },
+};
